1a06c55ec97247574b8d68d4341e184b095105cb
ZShUn <616394599@qq.com>
2018-09-19 21:45:49 +0800

    no message

---
+++ src/main/java/com/fast/family/example/FastFamilyExampleApplication.java
@@ -0,0 +1,12 @@
+package com.fast.family.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFamilyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFamilyExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fast/family/example/FastFamilyExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fast.family.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastFamilyExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
