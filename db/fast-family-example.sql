2c621b61584c44cd9ae8f92e51e0625aeaf7f7a6
ZShUn <616394599@qq.com>
2018-09-19 22:23:06 +0800

    demo数据库文件

---
+++ db/fast-family-example.sql
@@ -0,0 +1,31 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50716
+ Source Host           : localhost:3306
+ Source Schema         : fast-family-example
+
+ Target Server Type    : MySQL
+ Target Server Version : 50716
+ File Encoding         : 65001
+
+ Date: 19/09/2018 22:21:39
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` bigint(20) NOT NULL,
+  `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
