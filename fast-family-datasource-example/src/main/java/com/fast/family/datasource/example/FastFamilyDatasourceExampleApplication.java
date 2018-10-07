f243c2c2f7b5d1ca410ba1adab34852ec773087d
ZShUn <616394599@qq.com>
2018-10-07 15:13:04 +0800

    多数据源测试

--- fast-family-datasource-example/src/main/java/com/fast/family/datasource/example/FastFamilyDatasourceExampleApplication.java
+++ fast-family-datasource-example/src/main/java/com/fast/family/datasource/example/FastFamilyDatasourceExampleApplication.java
@@ -1,10 +1,24 @@
 package com.fast.family.datasource.example;
 
+import com.fast.family.datasource.DynamicDataSourceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.fast.family")
 @SpringBootApplication
 public class FastFamilyDatasourceExampleApplication {
+
+	@Autowired
+	private DynamicDataSourceProperties properties;
+
+
+	@GetMapping("/helloword")
+	public void helloword(){
+		System.out.println(properties);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FastFamilyDatasourceExampleApplication.class, args);
--- fast-family-datasource-example/src/test/java/com/fast/family/datasource/example/FastFamilyDatasourceExampleApplicationTests.java
+++ fast-family-datasource-example/src/test/java/com/fast/family/datasource/example/FastFamilyDatasourceExampleApplicationTests.java
@@ -1,7 +1,9 @@
 package com.fast.family.datasource.example;
 
+import com.fast.family.datasource.example.mapper.UserDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FastFamilyDatasourceExampleApplicationTests {
 
+	@Autowired
+	private UserDao userDao;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("查询数据大小："+userDao.selectAll().size());
+
 	}
 
 }
